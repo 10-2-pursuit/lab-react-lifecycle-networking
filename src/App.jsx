eda605918bc0475a4e9cd37979a07a9faed5d82c
elisaul batista <elisaulbatista@pursuit.org>
2023-09-07 20:19:46 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import StaffList from "./Components/StaffList";
 
 function App () {
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <StaffList />
     </>
   );
 };
