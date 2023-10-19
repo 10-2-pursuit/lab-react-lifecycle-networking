78cfd1c351bed630a80ca93f9f47de60cca777ee
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-19 08:32:10 -0400

    Add api fetch to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import { useEffect, useState } from "react";
 
-function App () {
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [pets, setPets] = useState([]);
+  useEffect(() => {
+    fetch("https://one0-2-vet-api.onrender.com/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data);
+      });
+    fetch("https://one0-2-vet-api.onrender.com/api/pets")
+      .then((response) => response.json())
+      .then((data) => {
+        setPets(data);
+      });
+  }, []);
+  console.log(employees);
+  console.log(pets);
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} pets={pets} />
     </>
   );
-};
+}
 
 export default App;
