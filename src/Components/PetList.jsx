efed7eb754cd7a5e3e4e5cbc863abece0d7e8d5a
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-19 09:13:43 -0400

    Show pet list.

--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,9 +1,14 @@
-export const PetList = () => {
-  return (
-    <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
-    </aside>
+export const PetList = ({ pets }) => {
+  return pets.length <= 0 ? (
+    <p>No pets listed for this employee.</p>
+  ) : (
+    <>
+      {pets.map((p) => (
+        <p>{p.name}</p>
+      ))}
+    </>
   );
+  return <aside className="pets-list"></aside>;
 };
 
 export default PetList;
