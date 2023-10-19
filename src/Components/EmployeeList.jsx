d63223ad15a6152cd911239d068f2aeb1cdde3f6
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-19 08:45:26 -0400

    Make employee list handle employees properly.

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
-export const EmployeeList = () => {
+export const EmployeeList = ({ employees, pets }) => {
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((e) => {
+          let petlist = pets.filter((pet) => pet.employeeId === e.id);
+          console.log(e, petlist);
+          return <Employee pets={petlist} info={e} />;
+        })}
       </section>
     </main>
   );
