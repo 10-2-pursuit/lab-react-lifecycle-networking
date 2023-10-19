f67c2a6e1a52a11c5771583bd141621ebc51615f
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-19 09:22:11 -0400

    Make button toggle petlist.

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,15 +1,23 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
 export const Employee = ({ pets, info }) => {
+  const [petsVisible, setPetsVisible] = useState(false);
   return (
     <article className="employee">
       <h3>{`${info.prefix + " " ?? ""} ${info.firstName} ${info.lastName}${
         " " + info.postfix ?? ""
       }`}</h3>
       <h4>{info.title}</h4>
-      <button>Show Pets</button>
-      <PetList pets={pets} />
+      <button
+        onClick={() => {
+          setPetsVisible(!petsVisible);
+        }}
+      >
+        Show Pets
+      </button>
+      {petsVisible ? <PetList pets={pets} /> : <></>}
     </article>
   );
 };
